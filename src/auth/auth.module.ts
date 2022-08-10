47df3de2e4f15f00bf0117540ba5ced2ff7213c5
huanlirui <472496416@qq.com>
2022-08-10 12:01:16 +0800

    feat:add config module to use env variables connect db

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService]
+  providers: [AuthService]
 })
 export class AuthModule {}
